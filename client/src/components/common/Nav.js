524482f26d27825fbb2f49a54c841462b2b6d9e7
khoi <knguyen3531@gmail.com>
2024-02-12 17:48:28 +1100

    Functional Nav Bar

--- client/src/components/common/Nav.js
+++ client/src/components/common/Nav.js
@@ -1,19 +1,41 @@
-// client/src/components/common/Nav.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import './nav.css';
 import { useAuth } from '../../utils/AuthContext';
 
 function Nav() {
-    const { currentUser, logout } = useAuth();
+    const { user, logout } = useAuth();
+    const [showLogoutConfirmation, setShowLogoutConfirmation] = useState(false);
+    const history = useHistory();
+
+    const handleLogout = () => {
+        setShowLogoutConfirmation(true);
+    };
+
+    const confirmLogout = () => {
+        logout();
+        setShowLogoutConfirmation(false);
+        history.push('/');
+    };
+
+    const cancelLogout = () => {
+        setShowLogoutConfirmation(false);
+    };
 
     return (
         <nav>
             <Link to="/">Home</Link>
-            {currentUser ? (
+            {user ? (
                 <>
                     <Link to="/dashboard">Dashboard</Link>
-                    <button onClick={logout} style={{ marginLeft: "20px", background: "none", border: "none", color: "#757575", cursor: "pointer" }}>Logout</button>
+                    <button onClick={handleLogout} style={{ marginLeft: "20px", background: "none", border: "none", color: "#757575", cursor: "pointer" }}>Logout</button>
+                    {showLogoutConfirmation && (
+                        <div style={{ marginTop: "10px" }}>
+                            <p>Are you sure you want to logout?</p>
+                            <button onClick={confirmLogout}>Yes</button>
+                            <button onClick={cancelLogout} style={{ marginLeft: "10px" }}>No</button>
+                        </div>
+                    )}
                 </>
             ) : (
                 <Link to="/login">Login</Link>
